6eb003db2356aff11c5c10587698461d02012494
alejmteo <aleemateoo29@gmail.com>
2026-05-10 18:57:31 +0200

    Anotaciones

--- reference_generator/send_trajectory.cpp
+++ reference_generator/send_trajectory.cpp
@@ -366,7 +366,7 @@ int main(int argc, char **argv)
 
     // Exercise 2 : Cartesian trajectory generation
     int tau = 1; //original 1
-    int T = 100;  //original 10
+    int T = 10;  //TIEMPO TOTAL DE MOVIMIENTO, original 10
     bool exercise_2 = true; // Set to true to execute Exercise 2
 
     if (exercise_2)
